0be11c5c10b836b138d32b2644167b9ce6f2cc44
Carsten Schipke <carsten.schipke@gmail.com>
2016-01-16 17:28:50 +0800

    Removed references to old import scriptlets.

--- config/default.php
+++ config/default.php
@@ -8,9 +8,5 @@ namespace Components;
   {
     I18n_Scriptlet_Import_Cldr_Common::serve('import/cldr/common');
     I18n_Scriptlet_Import_Cldr_Script::serve('import/cldr/script');
-
-    I18n_Scriptlet_Import_Geo_Cities::serve('import/geo/cities');
-    I18n_Scriptlet_Import_Geo_Regions::serve('import/geo/regions');
-    I18n_Scriptlet_Import_Geo_Regions_Translations::serve('import/geo/regions/translations');
   }
 ?>
